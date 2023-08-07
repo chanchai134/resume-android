1e97ebd1be425b64366292840b2d2ee1c69a032e
chanchai134 <chanchai_pom@hotmail.com>
2023-08-07 20:39:14 +0700

    rename activity

--- app/src/main/java/chanchai134/resume/ResumeActivity.kt
+++ app/src/main/java/chanchai134/resume/ResumeActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import chanchai134.resume.ui.theme.ResumeandroidTheme
 
-class MainActivity : ComponentActivity() {
+class ResumeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
